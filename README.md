71ff2f933b453add3966278041e76bd7fb4fa985
Dusan Dimitric <dusan@mitipi.com>
2019-05-15 10:29:45 +0200

    Add more info / format code

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 [![Build Status](https://travis-ci.org/DusanDimitric/node-auth-example.svg?branch=master)](https://travis-ci.org/DusanDimitric/node-auth-example)
 
-# Node.js authentication/authorization example
+# Node.js authentication/authorization example using JWT
 
 ```
 npm install
 npm test
 ```
+
+## Problems
+
+* Token can go stale
+* No Session/Refresh Token Mechanism
--- app/main.js
+++ app/main.js
@@ -12,15 +12,15 @@ app.use(bodyParser.json())
 
 app.post('/login', userController.logIn)
 
-app.post('/users', userController.create)
-app.get('/users',        authorize(ROLES.ADMIN), userController.getAll)
-app.get('/users/:id',    authorize(ROLES.USER), userController.getOne)
-app.put('/users/:id',    authorize(ROLES.USER), userController.update)
-app.delete('/users/:id', authorize(ROLES.USER), userController.deleteOne)
+app.post(  '/users', userController.create)
+app.get(   '/users',     authorize(ROLES.ADMIN), userController.getAll)
+app.get(   '/users/:id', authorize(ROLES.USER),  userController.getOne)
+app.put(   '/users/:id', authorize(ROLES.USER),  userController.update)
+app.delete('/users/:id', authorize(ROLES.USER),  userController.deleteOne)
 
 app.listen(
   app.get('port'),
-  () => console.log(`Example app listening on port ${app.get('port')}!`)
+  () => process.stdout.write(`Example app listening on port ${app.get('port')}!\n`)
 )
 
 module.exports = {
